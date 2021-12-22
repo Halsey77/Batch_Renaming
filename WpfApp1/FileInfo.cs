c11141a16ea4c55ec7785d32fc8ae9b3894f3cdc
Halsey77 <provcc143300@gmail.com>
2021-12-22 14:19:39 +0700

    thêm lớp FileInfo

---
+++ WpfApp1/FileInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace WpfApp1
+{
+    public class FileInfo: INotifyPropertyChanged
+    {
+        public string _name;
+        public string _newName;
+        public string _status;
+        public string _directory;
+        public string _extension;
+
+        public FileInfo(string name, string newName, string status, string directory, string extension)
+        {
+            _name = name;
+            _newName = newName;
+            _status = status;
+            _directory = directory;
+            _extension = extension;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
